17d756aa6f9e87f704c2f925172db838882c72f6
Julie A. Berry <jberry@student.fullsail.edu>
2019-02-26 12:21:30 -0800

    added UI elements

--- Simple Subtraction Tester/Simple Subtraction Tester/ViewController.swift
+++ Simple Subtraction Tester/Simple Subtraction Tester/ViewController.swift
@@ -9,7 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // OUTLETS
+    @IBOutlet weak var lblTitleLabelOUTLET: UILabel!
+    @IBOutlet weak var sgmDifficultyChoiceOUTLET: UISegmentedControl!
+    @IBOutlet weak var lblQuestionOUTLET: UILabel!
+    @IBOutlet weak var txtInputOUTLET: UITextField!
+    @IBOutlet weak var btnAnswerCheckOUTLET: UIButton!
+    @IBOutlet weak var lblScoreOUTLET: UILabel!
+    
+    // GLOBAL VARIABLES
+    
+    
+    // ACTIONS
+   
+    // segmented control
+    @IBAction func chooseDifficultyACTION(_ sender: Any) {
+        
+    }
+    
+    // button
+    @IBAction func checkAnswerACTION(_ sender: Any) {
+    }
+    
+    
+    // FUNCTIONS
 
+    // OVERRIDES    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
