ac337b4f63086a5718a87371325140f82762a221
Julie A. Berry <jberry@student.fullsail.edu>
2019-02-26 16:08:11 -0800

    implemented functionality

--- Simple Subtraction Tester/Simple Subtraction Tester/ViewController.swift
+++ Simple Subtraction Tester/Simple Subtraction Tester/ViewController.swift
@@ -19,6 +19,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var lblScoreOUTLET: UILabel!
     
     // GLOBAL VARIABLES
+    var randomNumber1 : Int?
+    var randomNumber2 : Int?
+    var lastRandomNumber : Int?
+    var correctAnswer : Int?
+    var userInput : Int?
+    var score : Int = 0
+    var questionAnswered : Int = 0
     
     
     // ACTIONS
@@ -34,13 +41,64 @@ class ViewController: UIViewController {
     
     
     // FUNCTIONS
+    func chooseEasyRandomNumbers(){
+        // generate a random number less than 10
+        randomNumber1 = Int(arc4random_uniform(10))
+        randomNumber2 = Int(arc4random_uniform(10))
+        // prevent the same numbers from coming up more than once consecutively - nice but not necessary
+        if (randomNumber1 == lastRandomNumber || randomNumber2 == lastRandomNumber || randomNumber1! < randomNumber2!){
+            chooseEasyRandomNumbers()
+        }
+        lastRandomNumber = randomNumber1
+        // note: random numbers will be displayed in displayQuestion()
+        
+        // assign the answer
+        correctAnswer = randomNumber1! - randomNumber2!
+    }
+    
+    func displayEasyQuestion(){
+         // generate random numbers and assign their values
+        chooseEasyRandomNumbers()
+        // assign the question text to a string  - to the question label - rn1 - rn2 = ?
+        lblQuestionOUTLET.text = String(randomNumber1!) + " - " + String(randomNumber2!) + " = ?"
+    }
+    
+    func chooseHardRandomNumbers(){
+        // generate a random number between 10 and 20
+        randomNumber1 = 10 - Int(arc4random_uniform(20))
+        randomNumber2 = 10 - Int(arc4random_uniform(20))
+        // prevent the same numbers from coming up more than once consecutively - nice but not necessary
+        if (randomNumber1 == lastRandomNumber || randomNumber2 == lastRandomNumber || randomNumber1! < randomNumber2!){
+            chooseHardRandomNumbers()
+        }
+        lastRandomNumber = randomNumber2
+        correctAnswer = randomNumber1! - randomNumber2!
+    }
+    
+    func displayHardQuestion(){
+        // generate random numbers and assign their values
+        chooseHardRandomNumbers()
+        // assign the question text to a string  - to the question label - rn1 - rn2 = ?
+        lblQuestionOUTLET.text = String(randomNumber1!) + " - " + String(randomNumber2!) + " = ?"
+        
+    }
+    
+    func checkAnswer() -> Bool {
+        // check user input agains correct answer
+        if (userInput! == correctAnswer) {
+            return true
+        } else {
+            return false
+        }
+    }
 
-    // OVERRIDES    
+    
+    
+    // OVERRIDES
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
 }
 
