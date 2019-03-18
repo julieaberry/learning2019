2177fbe28c447ab549f93ad44051eeb5dde3b396
Julie A. Berry <jberry@student.fullsail.edu>
2019-03-17 18:09:30 -0700

    add new project w/ ui elements

---
+++ Simple Algebra Test Tutorial/Simple Algebra Test Tutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Simple Algebra Test Tutorial
+//
+//  Created by Julie Berry on 3/17/19.
+//  Copyright © 2019 Julie Berry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
