9d2f43a6d2bad9bed7e8026705cb9e4d4c3f807c
Julie A. Berry <jberry@student.fullsail.edu>
2019-03-18 13:39:06 -0700

    add UI elements to view controller

--- Simple Algebra Test Tutorial/Simple Algebra Test Tutorial/ViewController.swift
+++ Simple Algebra Test Tutorial/Simple Algebra Test Tutorial/ViewController.swift
@@ -9,7 +9,61 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    // OUTLETS
+    
+    @IBOutlet weak var lblTitleOUTLET: UILabel!
+    @IBOutlet weak var lblQuestionOUTLET: UILabel!
+    @IBOutlet weak var txtInputOUTLET: UITextField!
+    @IBOutlet weak var lblRightOrWrongOUTLET: UILabel!
+    @IBOutlet weak var btnCheckOUTLET: UIButton!
+    @IBOutlet weak var pgvProgressViewOUTLET: UIView!
+    
+    
+    // GLOBAL VARIABLES
+    
+    
+    
+    // ACTIONS
+    
+    // button
+    @IBAction func checkIfCorrectACTION(_ sender: UIButton) {
+        // what happens when the button is pressed
+        
+        // takes in user input from text field
+        
+        // perform a check to see if answer is correct
+            // if correct
+                // increase score
+                // increase progress bar
+                // display message to user
+                // load the next question
+            // if incorrect
+                // increase progress bar
+                // display message to user
+                // load the next question
+            // if nothing is entered
+                // nothing happens
+    }
+    
+    
+    // FUNCTIONS
+    
+    // input text
+    
+    // button
+    
+    // progress bar
+    
+    //
+    
+    
+    
+    
+    
+    // OVERRIDES
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
