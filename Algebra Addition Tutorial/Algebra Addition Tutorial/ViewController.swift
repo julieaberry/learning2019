0959522f7de46690024b0da2e46f4654b3278a02
Julie A. Berry <jberry@student.fullsail.edu>
2019-03-19 12:34:20 -0700

    add UI elements to vc

--- Algebra Addition Tutorial/Algebra Addition Tutorial/ViewController.swift
+++ Algebra Addition Tutorial/Algebra Addition Tutorial/ViewController.swift
@@ -9,6 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    // OUTLETS
+    
+    @IBOutlet weak var lblTitleOUTLET: UILabel!
+    @IBOutlet weak var lblQuestionOUTLET: UILabel!
+    @IBOutlet weak var txtInputOUTLET: UITextField!
+    @IBOutlet weak var btnCheckAnswerOUTLET: UIButton!
+    @IBOutlet weak var lblRightOrWrongOUTLET: UILabel!
+    @IBOutlet weak var lblProgressOUTLET: UILabel!
+    
+    
+    // GLOBAL VARIABLES
+    
+    var randomNumber : Int?
+    var lastRandomNumber : Int?
+    var userAnswer : Int?
+    var correctAnswer : Int?
+    var progress : Int = 0
+    var difficulty : String = "easy"
+    
+    
+    // ACTIONS
+    
+    @IBAction func checkAnswerACTION(_ sender: UIButton) {
+        // check for input
+        // cast input as Int
+        // check if input is true
+        // auto remove input
+        
+    }
+    
+    
+    // FUNCTIONS
+    
+    
+    
+    
+    // OVERRIDES
 
     override func viewDidLoad() {
         super.viewDidLoad()
