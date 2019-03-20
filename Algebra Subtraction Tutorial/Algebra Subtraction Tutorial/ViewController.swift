8e4b79272a9f11656ecdd3b76fb997a5747fd5b7
Julie A. Berry <jberry@student.fullsail.edu>
2019-03-19 18:54:02 -0700

    add new project with UI elements

---
+++ Algebra Subtraction Tutorial/Algebra Subtraction Tutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Algebra Subtraction Tutorial
+//
+//  Created by Julie Berry on 3/19/19.
+//  Copyright © 2019 Julie Berry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
