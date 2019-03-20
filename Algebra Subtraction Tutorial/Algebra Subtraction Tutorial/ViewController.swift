54bc0e408739cef0be2cff9d040da3447941373b
Julie A. Berry <jberry@student.fullsail.edu>
2019-03-19 19:29:33 -0700

    add UI elements to vc

--- Algebra Subtraction Tutorial/Algebra Subtraction Tutorial/ViewController.swift
+++ Algebra Subtraction Tutorial/Algebra Subtraction Tutorial/ViewController.swift
@@ -9,6 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    // OUTLETS
+    
+    @IBOutlet weak var lblTitle: UILabel!
+    @IBOutlet weak var lblQuestion: UILabel!
+    @IBOutlet weak var txtInput: UITextField!
+    @IBOutlet weak var btnCheckAnswer: UIButton!
+    @IBOutlet weak var btnNo: UIButton!
+    @IBOutlet weak var lblRightOrWrong: UILabel!
+    @IBOutlet weak var lblProgress: UILabel!
+    
+    
+    // GLOBAL VARIABLES
+    
+    
+    
+    
+    
+    // ACTIONS
+    
+    @IBAction func checkAnswerACTION(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func chooseNoACTION(_ sender: UIButton) {
+        
+    }
+    
+    // FUNCTIONS
+    
+    
+    
+    // OVERRIDES
 
     override func viewDidLoad() {
         super.viewDidLoad()
