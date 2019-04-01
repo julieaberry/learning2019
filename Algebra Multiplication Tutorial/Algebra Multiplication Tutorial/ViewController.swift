af81738167cedefed3d9b65fcc037757c8c789e7
Julie A. Berry <jberry@student.fullsail.edu>
2019-04-01 10:01:51 -0700

    add new project

--- Algebra Multiplication Tutorial/Algebra Multiplication Tutorial/ViewController.swift
+++ Algebra Multiplication Tutorial/Algebra Multiplication Tutorial/ViewController.swift
@@ -9,6 +9,56 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    /*
+     
+     SWITCH begins in off position
+     
+     App displays a question, takes in a response, calculates the response anse informs the usei of right or wrong anser/
+     
+     App keeps track of responses using a progress view.
+     When progress is full, difficulty level is increased up to 3 levels
+     
+     
+     
+     
+     
+ */
+    
+    
+    // OUTLETS
+    
+    @IBOutlet weak var lblQuestionLabel: UILabel!
+    @IBOutlet weak var txtInputText: UITextField!
+    @IBOutlet weak var btnCheckAnswerButton: UIButton!
+    @IBOutlet weak var lblIsCorrectLabel: UILabel!
+    @IBOutlet weak var pgvProgressView: UIProgressView!
+    @IBOutlet weak var swcNegativeSwitch: UISwitch!
+    @IBOutlet weak var lblSwitchLabel: UILabel!
+    
+    
+    // GLOBAL VARIABLES
+    
+    
+    
+    // ACTIONS
+    
+    
+    @IBAction func pressCheckAnswerButton(_ sender: UIButton) {
+        
+    }
+    
+     @IBAction func turnSwitchOnOrOff(_ sender: UISwitch) {
+        
+    }
+    
+    
+    
+    // FUNCTIONS
+    
+    
+    
+    // OVERRIDES
 
     override func viewDidLoad() {
         super.viewDidLoad()
